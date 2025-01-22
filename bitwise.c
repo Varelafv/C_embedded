c1097e6e994f7d844400f01e59dff1babc8b9550
Francisco Varela <fvarela@nt.lirmm.fr>
2025-01-22 12:18:22 +0100

    video finished

--- bitwise.c
+++ bitwise.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 void print_bin (short val);
 int main() {
-    unsigned char  PORT=0xCF ;
-    print_bin(~(1<<5));
-    if (PORT&(1<<6)) {
-        printf("True",PORT);
-    }
-    else 
-    printf("false");
+    unsigned char  PORT=0xFF ;
+    print_bin(PORT);
+    PORT ^= (1<<6)  | (1<<3);
+    print_bin(PORT);
+    PORT ^= (1<<6)  | (1<<3);
+    print_bin(PORT);
     return 0 ;
 
 }
