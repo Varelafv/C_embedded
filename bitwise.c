b243b3ff7fa1144c4acc97c180eb84232d7870d1
Francisco Varela <fvarela@nt.lirmm.fr>
2025-02-27 13:29:17 +0100

    Systick Timer

--- bitwise.c
+++ bitwise.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-void print_bin (short val);
-int main() {
-    short  PORTA=0x05 , PORTB = 0x04 ;
-    print_bin(PORTA);
-    print_bin(PORTB);
-    PORTA = PORTA & PORTB ;
-    printf( " %d",PORTA);
-    return 0 ;
-
-}
+ RCC->AHBENR |= ((1<<17) | (1<<19)) ;
+  GPIOC->MODER &= ~((1<<26)| (1<<27));
+  GPIOA->MODER &= ~(3U << (5 * 2));
+  GPIOA->MODER |= (1U << ( 5* 2));
+  while (1)
+  {
+	   SystickDelay(1000);
+    	GPIOA->ODR ^= (1<<5);
 
 
+  /* USER CODE END 3 */
+} }
 
-void print_bin(short val)
-{
-    short i ;
-    for (i=7;i>=0;i--)
-        printf("%c",(val&(1<<i))? '1':'0');
-    putchar('b');
-    putchar('\n');
+
+void SystickDelay (int ms ){
+	  int i = 0 ;
+	  SysTick->LOAD = 48000 - 1 ;
+	  SysTick->VAL = 0; //reset the systick counter flag
+	  SysTick->CTRL = ((1<<2) | (1<<0) | (1<<1)); //enable systick and select processor clock
+
+	  for (int i =0 ; i<ms;i++){
+		  while( (SysTick->CTRL & (1<<16)) == 0 ){
+
+		  }
+
+	  }
+
 }
